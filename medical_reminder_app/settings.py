f71d9dd861a591136b0197b78408433ef16066de
Shreyash Ingle <141054759+shreyash0019@users.noreply.github.com>
2025-11-23 22:34:55 +0530

    Add allowed host for production environment

--- medical_reminder_app/settings.py
+++ medical_reminder_app/settings.py
@@ -40,7 +40,12 @@ SECRET_KEY = 'django-insecure-a7_+bu%6ld(z)qbga6_o-xo%lnm9o#dofsx#trsdg-it*@r+di
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ "*" , '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    'reminder-xxxx.onrender.com'
+]
+
 
 
 
