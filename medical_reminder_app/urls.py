08cb1982503595d526dbb7f173a241954fdbc33d
Shreyash Ingle <141054759+shreyash0019@users.noreply.github.com>
2025-11-23 23:13:09 +0530

    Update urls.py

--- medical_reminder_app/urls.py
+++ medical_reminder_app/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({"message": "Medical Reminder API is running!"})
 
 urlpatterns = [
+    path('', home),   # 👈 add this line
     path('admin/', admin.site.urls),
-    #path("firebase/", include("firebase_users.urls")),
     path('api/accounts/', include('accounts.urls')),
     path('api/pharmacy/', include('pharmacy.urls')),
     path('api/reminders/', include('reminders.urls')),
