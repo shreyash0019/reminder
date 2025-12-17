181c1ffe4740913c9ae9fa8b6940cb7815e701a3
Shreyash Ingle <141054759+shreyash0019@users.noreply.github.com>
2025-12-18 00:54:54 +0530

    Update urls.py

--- reminders/urls.py
+++ reminders/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.reminder_list),
-    path("create/", views.create_reminder),
-    path("mark-taken/<int:reminder_id>/", views.mark_taken),
-    path("due/", views.due_reminders),
+    path('', views.reminder_list, name='reminder_list'),
+    path('create/', views.create_reminder, name='create_reminder'),
+    path('mark_taken/<int:reminder_id>/', views.mark_taken, name='mark_taken'),
+    path('due/', views.due_reminders, name='due_reminders'),
 ]
