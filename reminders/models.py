6089e8a74da3fcccbb635fce5ed53f5486566cd6
Shreyash Ingle <141054759+shreyash0019@users.noreply.github.com>
2025-12-17 23:01:51 +0530

    Update models.py

--- reminders/models.py
+++ reminders/models.py
@@ -1,30 +1,8 @@
 from django.db import models
-from django.conf import settings
 from pharmacy.models import Medicine
-
-
-class Device(models.Model):
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name="devices"
-    )
-    fcm_token = models.CharField(max_length=255, unique=True)
-    platform = models.CharField(max_length=20, default="android")
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f"{self.user} - {self.platform}"
-
 
 class Reminder(models.Model):
     firebase_patient_id = models.CharField(max_length=100)
-    caretaker = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True
-    )
     medicine = models.ForeignKey(Medicine, on_delete=models.CASCADE)
     dosage = models.CharField(max_length=50)
     reminder_time = models.DateTimeField()
