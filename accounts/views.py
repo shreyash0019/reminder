55edfed5d1b63192672d199b6e0ed74c24c89e41
Shreyash Ingle <141054759+shreyash0019@users.noreply.github.com>
2025-12-19 01:26:09 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,9 @@ from rest_framework.authtoken.models import Token
 from rest_framework.permissions import AllowAny
 from .serializers import UserSerializer, LoginSerializer
 from .models import User
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+
 
 
 # Register View
@@ -76,3 +79,45 @@ class CaretakerLoginView(generics.GenericAPIView):
                 status=status.HTTP_403_FORBIDDEN,
             )
         return generate_login_response(user)
+    
+    
+class PatientListView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        if request.user.role != "caretaker":
+            return Response({"error": "Only caretaker allowed"}, status=403)
+
+        patients = User.objects.filter(role="patient")
+
+        data = []
+        for p in patients:
+            data.append({
+                "id": p.id,
+                "username": p.username,
+                "fcm_token": p.fcm_token,
+            })
+
+        return Response(data)
+
+    
+    
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+
+class CaretakerDetailView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+
+        if user.role != "caretaker":
+            return Response({"error": "Not a caretaker"}, status=403)
+
+        return Response({
+            "id": user.id,
+            "username": user.username,
+            "role": user.role,
+            "fcm_token": user.fcm_token,
+        })
+
