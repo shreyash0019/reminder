b461ff518402ab50e356abfb8321bdfa41121807
Shreyash Ingle <141054759+shreyash0019@users.noreply.github.com>
2026-02-05 05:02:58 +0530

    Update urls.py

--- pharmacy/urls.py
+++ pharmacy/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import (
     medicine_list, medicine_detail, medicine_create_update,
-    record_sale, low_stock, expired_medicines
+    record_sale, low_stock, expired_medicines, daily_sales_report ,
 )
 
 urlpatterns = [
@@ -11,4 +11,5 @@ urlpatterns = [
     path("sale/", record_sale),
     path("medicines/low-stock/", low_stock),
     path("medicines/expired/", expired_medicines),
+    path("sales/daily/", daily_sales_report, name="daily_sales_report"),
 ]
