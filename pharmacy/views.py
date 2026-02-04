ab7bfd1b44c09e8f13dc98d3b43efda4d127a895
Shreyash Ingle <141054759+shreyash0019@users.noreply.github.com>
2026-02-05 05:01:52 +0530

    Update views.py

--- pharmacy/views.py
+++ pharmacy/views.py
@@ -81,3 +81,40 @@ def expired_medicines(request):
     expired = [m for m in Medicine.objects.all() if m.is_expired()]
     serializer = MedicineSerializer(expired, many=True)
     return Response(serializer.data)
+
+
+from django.http import JsonResponse
+from django.utils.timezone import now
+from .models import Sale
+
+def daily_sales_report(request):
+    today = now().date()
+
+    sales = Sale.objects.filter(sale_date__date=today)
+
+    total_amount = 0
+    total_quantity = 0
+    sales_data = []
+
+    for sale in sales:
+        amount = sale.quantity * sale.price
+        total_amount += amount
+        total_quantity += sale.quantity
+
+        sales_data.append({
+            "medicine": sale.medicine.name,
+            "company": sale.medicine.company,
+            "quantity_sold": sale.quantity,
+            "price_per_unit": sale.price,
+            "amount": amount,
+            "sale_time": sale.sale_date.strftime("%H:%M:%S")
+        })
+
+    return JsonResponse({
+        "date": str(today),
+        "total_sales_amount": total_amount,
+        "total_quantity_sold": total_quantity,
+        "total_transactions": sales.count(),
+        "sales": sales_data
+    })
+
