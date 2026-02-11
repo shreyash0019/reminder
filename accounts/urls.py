a6e7df2cf7ba898e1e51e2d4879677ce290aef02
Shreyash Ingle <141054759+shreyash0019@users.noreply.github.com>
2026-02-11 22:44:18 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,18 +1,20 @@
 from django.urls import path
-from .views import CaretakerDetailView, PatientListView
-
 from .views import (
     RegisterView,
+    SellerLoginView,
     PatientLoginView,
     CaretakerLoginView,
-    CaretakerDetailView,
     PatientListView,
+    CaretakerDetailView,
 )
 
 urlpatterns = [
-    path("register/", RegisterView.as_view()),
-    path("login/patient/", PatientLoginView.as_view()),
-    path("login/caretaker/", CaretakerLoginView.as_view()),
-    path("caretaker/me/", CaretakerDetailView.as_view()),
-    path("caretaker/patients/", PatientListView.as_view()),
+    path('register/', RegisterView.as_view(), name='register'),
+
+    path('seller/login/', SellerLoginView.as_view(), name='seller-login'),
+    path('patient/login/', PatientLoginView.as_view(), name='patient-login'),
+    path('caretaker/login/', CaretakerLoginView.as_view(), name='caretaker-login'),
+
+    path('caretaker/patients/', PatientListView.as_view(), name='caretaker-patients'),
+    path('caretaker/me/', CaretakerDetailView.as_view(), name='caretaker-detail'),
 ]
