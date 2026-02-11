993e27b57913ad088c665bffc8847c7af31237e4
Shreyash Ingle <141054759+shreyash0019@users.noreply.github.com>
2026-02-12 01:31:40 +0530

    Update urls.py

--- pharmacy/urls.py
+++ pharmacy/urls.py
@@ -1,15 +1,16 @@
-from django.urls import path
-from .views import (
-    medicine_list, medicine_detail, medicine_create_update,
-    record_sale, low_stock, expired_medicines, daily_sales_report ,
-)
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from .views import MedicineViewSet, SaleViewSet, low_stock, expired_medicines, daily_sales_report
+
+router = DefaultRouter()
+router.register(r'medicines', MedicineViewSet, basename='medicines')
+router.register(r'sales', SaleViewSet, basename='sales')
 
 urlpatterns = [
-    path("medicines/", medicine_list),
-    path("medicines/<int:pk>/", medicine_detail),
-    path("medicines/save/", medicine_create_update),
-    path("sale/", record_sale),
-    path("medicines/low-stock/", low_stock),
-    path("medicines/expired/", expired_medicines),
-    path("sales/daily/", daily_sales_report, name="daily_sales_report"),
+    path('', include(router.urls)),
+
+    # Custom APIs
+    path('medicines/low-stock/', low_stock),
+    path('medicines/expired/', expired_medicines),
+    path('sales/daily/', daily_sales_report),
 ]
