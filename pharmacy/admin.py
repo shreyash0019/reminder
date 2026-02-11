74528b2d8b6a55b6c138e4d37a9e4b09fc91e738
Shreyash Ingle <141054759+shreyash0019@users.noreply.github.com>
2026-02-12 01:59:52 +0530

    Update admin.py

--- pharmacy/admin.py
+++ pharmacy/admin.py
@@ -9,5 +9,6 @@ class MedicineAdmin(admin.ModelAdmin):
 
 @admin.register(Sale)
 class SaleAdmin(admin.ModelAdmin):
-    list_display = ("medicine", "quantity_sold", "sale_date")
-    list_filter = ("sale_date",)
+    list_display = ("medicine", "quantity", "price", "sale_date")  # fixed
+    list_filter = ("sale_date", "medicine")
+    search_fields = ("medicine__name",)
