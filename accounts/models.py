62378c9287bde835500b05c90b7fcf42515e10de
Shreyash Ingle <141054759+shreyash0019@users.noreply.github.com>
2026-02-11 22:35:59 +0530

    Add default value to user role field

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,20 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
+
     ROLE_CHOICES = (
+        ('seller', 'Seller'),
         ('patient', 'Patient'),
         ('caretaker', 'Caretaker'),
     )
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default='patient'   # prevents migration errors
+    )
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
